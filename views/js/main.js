107f3092663387be623d8827c08481c86f123f09
Chris Murone <chris.murone@ge.com>
2017-05-03 15:21:07 -0400

    Optimize pizza movements.

--- views/js/main.js
+++ views/js/main.js
@@ -422,7 +422,7 @@ var resizePizzas = function(size) {
   changeSliderLabel(size);
 
 
-  //Sets width variable in % based on slider. 
+  //Sets width variable in % based on slider.
   function changePizzaSizes(size) {
     switch(size) {
       case "1":
@@ -491,8 +491,9 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
+  var pos = document.body.scrollTop/1250
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    var phase = Math.sin((pos) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
@@ -507,21 +508,24 @@ function updatePositions() {
 }
 
 // runs updatePositions on scroll
-// window.addEventListener('scroll', updatePositions);
+window.addEventListener('scroll', updatePositions);
 
 // Generates the sliding pizzas when the page loads.
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  for (var i = 0; i < 200; i++) {
-    var elem = document.createElement('img');
+  var piznum = Math.ceil((screen.height / s) * cols);
+  var elem;
+  var movingPizzas = document.querySelector("#movingPizzas1")
+  for (var i = 0; i < piznum; i++) {
+    elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
     elem.style.height = "100px";
     elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
-    document.querySelector("#movingPizzas1").appendChild(elem);
+    movingPizzas.appendChild(elem);
   }
   updatePositions();
 });
